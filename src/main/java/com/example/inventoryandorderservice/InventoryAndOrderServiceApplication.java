d3b272d200fe10a8f009803ae1ea134286928454
MANOJ S B <manojsb5112@gmail.com>
2024-10-18 19:20:57 +0530

    initial commit

---
+++ src/main/java/com/example/inventoryandorderservice/InventoryAndOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventoryandorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAndOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAndOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inventoryandorderservice/InventoryAndOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryandorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAndOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
